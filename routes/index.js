4c1fed2b3b5db25783928c3875a7397c1c624fcb
Carol Nguyen <cthoa01@gmail.com>
2021-03-17 00:47:25 -0700

    update template

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 router.use('/api', require('./noteRoutes.js'))
+//index needs to be rendered last
 router.use('/', require('./viewRoutes.js'))
 
 module.exports = router
